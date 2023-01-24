ebe65f1e99b8f332e35d03cd4a3bfbe66ad48bbf
Mohamed Eldemellawy <123407618+Mohamedeldemellay@users.noreply.github.com>
2023-01-25 00:55:39 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import data.string;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File selectedfile = new File ("student-data.txt");
+             Scanner scanner = new Scanner(selectedfile);
+
+    private static void While (Scanner.hasNext()) {
+        string data = scanner.nextLine().replace()
+        }
+    }
+
+ /* Sorry, but i can’t undersatand.
+    i will study again, and i will deliver in a few days.
+  */
